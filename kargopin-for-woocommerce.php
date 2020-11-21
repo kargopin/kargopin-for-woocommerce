f8d0384fc5d67bc61144aa85b94936758947f53e
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2020-11-21 11:57:48 +0300

    Plugin init

---
+++ kargopin-for-woocommerce.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Plugin Name: Kargopin for WooCommerce
+* Description: Kargo entegrasyonunun kolay yolu!
+* Version: 0.0.0
+* Author: Kargopin
+* Author URI: https://www.kargopin.com/
+* Text Domain: kargopin-for-woocommerce
+* Domain Path: /languages
+* WC requires at least: 4.0.0
+* WC tested up to: 4.7.0
+ */
