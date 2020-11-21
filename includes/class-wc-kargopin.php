40f14d5a57fe5dd13ad212f893d9d1e8fc29e325
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2020-11-21 12:08:16 +0300

    WC_KargoPin class was created.

---
+++ includes/class-wc-kargopin.php
@@ -0,0 +1,18 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+final class WC_KargoPin {
+
+    public function __construct()
+    {
+        
+    }
+
+    public function includes()
+    {
+        
+    }
+}
+
+new WC_KargoPin();
--- kargopin-for-woocommerce.php
+++ kargopin-for-woocommerce.php
@@ -11,3 +11,11 @@
 * WC requires at least: 4.0.0
 * WC tested up to: 4.7.0
  */
+
+ if ( ! defined( 'WC_KARGOPIN_PLUGIN_FILE' ) ) {
+     define( 'WC_KARGOPIN_PLUGIN_FILE', __FILE__ );
+ }
+
+ if ( ! class_exists( 'WC_KargoPin' ) ) {
+     include_once dirname( WC_KARGOPIN_PLUGIN_FILE ) . '/includes/class-wc-kargopin.php';
+ }
