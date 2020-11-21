fceb0edfea95e23bd5c206be54b4f04020381919
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2020-11-21 13:00:57 +0300

    DataEncryption class created.

---
+++ includes/Storage/Data_Encryption.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KP\Storage;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Data_Encryption {
+    
+}
--- includes/class-wc-kargopin.php
+++ includes/class-wc-kargopin.php
@@ -6,12 +6,12 @@ final class WC_KargoPin {
 
     public function __construct()
     {
-        
+        $this->includes();
     }
 
     public function includes()
     {
-        
+        include_once WC_KARGOPIN_PATH . 'includes/Storage/Data_Encryption.php';
     }
 }
 
