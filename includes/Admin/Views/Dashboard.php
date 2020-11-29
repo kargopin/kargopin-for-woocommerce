0c020a50e21122c84914cd24c48865ae5e52c309
Mustafa Şükrü Kapusuz <mustafa@intense.com.tr>
2020-11-29 19:32:57 +0300

    Dashboard style was updated.

--- includes/Admin/Views/Dashboard.php
+++ includes/Admin/Views/Dashboard.php
@@ -1,32 +1,24 @@
 <h2><?php echo __( 'Account Settings', 'kargopin-for-woocommerce' ); ?></h2>
+<div class="card">
+    <h2 class="title">Connect your WooCommerce Store to KargoPin!</h2>
+	    <p>
+            <?php
+                if( $data['customer_data']['status'] == 200 ){
+            ?>
+                <h3><?php echo __( 'Hi,', 'kargopin-for-woocommerce' ); ?> <?php printf( "%s %s", $data['customer_data']['data']->data->firstname, $data['customer_data']['data']->data->lastname ); ?>!</h3>
+            <?php
+                }else{
+            ?>
+                    <form action="" method="POST">
+                        <?php wp_nonce_field( 'kargopin_oauth_login', 'security' ); ?>
 
-<div id="poststuff">
-    <div id="post-body" class="metabox-holder columns-2">
-        <div id="post-body-content">
-            <div class="postbox">
-                <h2 class="title">Connect your WooCommerce Store to KargoPin!</h2>
-
-                <div class="inside">
-                    <?php
-                        if( $data['customer_data']['status'] == 200 ){
-                    ?>
-                        <h3><?php echo __( 'Hi,', 'kargopin-for-woocommerce' ); ?> <?php printf( "%s %s", $data['customer_data']['data']->data->firstname, $data['customer_data']['data']->data->lastname ); ?>!</h3>
-                    <?php
-                        }else{
-                    ?>
-                            <form action="" method="POST">
-                                <?php wp_nonce_field( 'kargopin_oauth_login', 'security' ); ?>
-
-                                <?php submit_button(
-                                    __( 'Login to your Kargopin Account', 'kargopin-for-woocommerce' ),
-                                    'primary',
-                                    'submit'
-                                ); ?>
-                            </form>
-                    
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-    </div>
+                        <?php submit_button(
+                            __( 'Login to your Kargopin Account', 'kargopin-for-woocommerce' ),
+                            'primary',
+                            'submit'
+                        ); ?>
+                    </form>
+            
+            <?php } ?>
+        </p>
 </div>
